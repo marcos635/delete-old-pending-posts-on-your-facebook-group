394a201109f6146ffc2d542200b72d328d27fa2b
marcos635 <marcosvinicius635@gmail.com>
2020-07-03 10:52:16 -0300

    Update delete-pending-posts-on-facebook.js

--- delete-pending-posts-on-facebook.js
+++ delete-pending-posts-on-facebook.js
@@ -1,10 +1,21 @@
 //ENSURE YOU ARE LOGGED IN AS AN THE GROUP'S MODERATOR OR ADMIN AND NAVIGATE TO THE PENDING POSTS PAGE
 //PRESS CTRL + SHIFT + I TO OPEN THE BROWSER CONSOLE AND PASTE THESE FOLLOWING CODES LINE BY LINE
 
-let deleteButtons = document.querySelectorAll('.sx_f437f9'); deleteButtons.forEach(btn => btn.click());
-//select all delete buttons on screen, bot clicks all the selected delete buttons
+function deleteButtons() {
+    var recusar = document.querySelectorAll('.sx_67dd83');
+    recusar.forEach(btn => btn.click());
+}
+//function select all delete buttons on screen, bot clicks all the selected delete buttons
 
-let del = document.querySelectorAll('.layerConfirm'); del.forEach(btn => btn.click());
-//this line selects all delete confirmation that appears on screen. Bot automatically clicks and delete on all the modal pop-ups on screen
+function confirmdelete() {
+    var excluir = document.querySelectorAll('.layerConfirm'); 
+    excluir.forEach(btn => btn.click());
+}
+//function selects all the delete confirmation buttons on the screen, bot clicks on all selected delete confirmation buttons.
+
+var del = setInterval( deleteButtons, 8000);
+//Automatically clicks and waits 8 seconds to perform the next function
+
+var confirm = setInterval( confirmdelete, 3000);
 
 //Refresh the page and run the 
